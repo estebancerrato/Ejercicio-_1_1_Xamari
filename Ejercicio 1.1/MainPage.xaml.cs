fc2a597ad7603326b5cb5c0c8c50035bc951acf4
estebancerrato <estebancerrato@hotmail.com>
2022-06-17 22:52:00 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 1.1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio_1._1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            Double total = 0;
+            total = Double.Parse(txtnumero1.Text) + Double.Parse(txtnumero2.Text);
+            await Navigation.PushAsync(new resultado(total.ToString()));
+        }
+
+        private async void btnRestar_Clicked(object sender, EventArgs e)
+        {
+            Double total = 0;
+            total = Double.Parse(txtnumero1.Text) - Double.Parse(txtnumero2.Text);
+            await Navigation.PushAsync(new resultado(total.ToString()));
+        }
+
+        private async void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            Double total = 0;
+            total = Double.Parse(txtnumero1.Text) / Double.Parse(txtnumero2.Text);
+            await Navigation.PushAsync(new resultado(total.ToString()));
+        }
+
+        private async void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            Double total = 0;
+            total = Double.Parse(txtnumero1.Text) * Double.Parse(txtnumero2.Text);
+            await Navigation.PushAsync(new resultado(total.ToString()));
+        }
+    }
+}
---
+++ Ejercicio 1.1/resultado.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio_1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class resultado : ContentPage
+{
+    public resultado()
+    {
+        InitializeComponent();
+    }
+    public resultado(String resultado)
+    {
+        InitializeComponent();
+        lblMostrarResultado.Text = "El resultado es: "+ resultado;
+
+    }
+}
+}
